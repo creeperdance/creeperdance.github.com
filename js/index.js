f9bab347570defa42911dffade5c7f97aee31aef
creeperdance <creeperdance@163.com>
2017-05-14 23:47:24 +0800

    jekyll category

--- js/index.js
+++ js/index.js
@@ -7,4 +7,7 @@ $(function() {
 		$('#right_sidebar').animate({right:"-=300px"},300);
 		$('#content').animate({left:'+=150px'},300);
 	});
+	$('.list-category h2').hover(function(){
+		$(this).css('border-bottom','2px solid #555');
+	});
 });
